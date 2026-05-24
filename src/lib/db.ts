33ac42bfd2a595ee1f6ceedb4ecc46d8c364f1a2
rah-shresh <shresrahangdale@gmail.com>
2026-05-24 21:29:03 +0530

    fix prisma setup

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+// 1. Change this line (remove /edge)
+import { PrismaClient } from "@prisma/client"; 
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "@neondatabase/serverless"; // Edge-compatible driver
+import { Pool } from "@neondatabase/serverless"; 
 
 const prismaClientSingleton = () => {
-  // Build step ke liye fallback dummy URL
   const connectionString =
     process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy";
   
-  // Neon ka serverless pool use karein jo web sockets ke throw connect hota hai
   const pool = new Pool({ connectionString });
   const adapter = new PrismaPg(pool);
   
-  // Regular PrismaClient mein adapter pass karein
   return new PrismaClient({ adapter });
 };
 
