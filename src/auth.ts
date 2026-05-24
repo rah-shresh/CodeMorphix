9c80f870b0cd7ed269a89cb6e962e29b79ae519c
rah-shresh <shresrahangdale@gmail.com>
2026-05-24 23:29:30 +0530

    fix google auth db

--- src/auth.ts
+++ src/auth.ts
@@ -12,8 +12,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   },
   providers: [
     Google({
-      clientId: process.env.AUTH_GOOGLE_ID || "",
-      clientSecret: process.env.AUTH_GOOGLE_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
     Credentials({
       name: "Credentials",
