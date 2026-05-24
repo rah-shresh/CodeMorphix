d36cb6e40575dbebd1cd9a3183785e9efea877b6
rah-shresh <shresrahangdale@gmail.com>
2026-05-24 22:20:27 +0530

    final prisma fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,5 +8,6 @@ export const db =
   globalForPrisma.prisma ??
   new PrismaClient();
 
-if (process.env.NODE_ENV !== "production")
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = db;
+}
