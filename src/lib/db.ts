ddbadc5b42228d656909086e9abbd142d3e188c8
rah-shresh <shresrahangdale@gmail.com>
2026-05-24 21:19:53 +0530

    fix prisma import

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "@neondatabase/serverless"; // Edge-compatible driver
 
