d2d3c29146c60eca6989585a2a0d8c2ad81a01e0
rah-shresh <shresrahangdale@gmail.com>
2026-05-24 22:04:20 +0530

    simplify prisma client

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,22 +1,12 @@
-// 1. Change this line (remove /edge)
-import { PrismaClient } from "@prisma/client"; 
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "@neondatabase/serverless"; 
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  const connectionString =
-    process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy";
-  
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaPg(pool);
-  
-  return new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient();
 
-export const db = globalThis.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = db;
